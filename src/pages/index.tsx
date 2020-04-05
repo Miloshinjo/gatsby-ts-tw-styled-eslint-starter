d1809ba6c19b861b6027947a63ea86e7345e543a
Milos Dzeletovic <milosdzeletovic@Miloss-MacBook-Pro.local>
2020-04-05 14:24:22 +0200

    Update readme and style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import tw from 'twin.macro';
 
 import Layout from '../components/layout';
 
 import SEO from '../components/seo';
 
+const Title = styled.h1`
+  ${tw`text-4xl mb-2`}
+`;
+
+const Subtitle = styled.h2`
+  ${tw`text-3xl`}
+`;
+
+const Paragraph = styled.p`
+  ${tw`text-xl mb-8`}
+`;
+
 function IndexPage(): JSX.Element {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <h2>
+      <Title>Hi people</Title>
+      <Subtitle>
         This is a quick Typescript, @emotion/styled, TailwindCSS, eslint Gatsby
         starter.
-      </h2>
-      <p>Now go build something great.</p>
+      </Subtitle>
+      <Paragraph>Now go build something great.</Paragraph>
     </Layout>
   );
 }
